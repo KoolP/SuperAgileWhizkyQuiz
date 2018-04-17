5557f895cee2f80018027dc67d7e6040a289a42e
Victoria Grönqvist <victoriagronqvist@victorias-macbook-air.local>
2018-04-17 15:03:42 +0200

    Created some funcs

--- SuperAgileWhizkyQuiz/QuizViewController.swift
+++ SuperAgileWhizkyQuiz/QuizViewController.swift
@@ -9,54 +9,105 @@
 import UIKit
 
 class QuizViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var rightOrWrongLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var firstChoice: UIButton!
+    @IBOutlet weak var secondChoice: UIButton!
+    @IBOutlet weak var thirdChoice: UIButton!
+    @IBOutlet weak var fourthChoice: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    var scoreCounter : Int!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scoreCounter = 0
 
         //#warning load new question here
-        loadNewQuestion()
+        loadNewGame()
         
     }
     
-    func loadNewQuestion() {
-        
+    
+    @IBAction func newQuestionButton(_ sender: UIButton) {
+        loadNewQuestionAndAnswers()
+    }
+    
+    func loadNewQuestionAndAnswers() {
+        //put in QuizModels randomizer with existing game
+        showButtons()
+    }
+    
+    @IBAction func playAgainButton(_ sender: UIButton) {
+        loadNewGame()
+    }
+    
+    func loadNewGame() {
+        //put in QuizModels randomizer on all questions
+        showButtons()
     }
     
     func makeGuess() {
         
     }
     
+    func addScoreCount() {
+        if scoreCounter < 10 {
+            scoreCounter = scoreCounter + 1
+        } else {
+            hideButtons()
+        }
+    }
+    
     //#warning hide and disable buttons
-    func hideAnswer() {
+    func hideButtons() {
         hideAnswerButtons()
         disableAnswerButtons()
-        
     }
     
     //#warning show and enable buttons
-    func showAnswer() {
+    func showButtons() {
         showAnswerButtons()
         enableAnswerbuttons()
     }
     
     //Hide buttons
     func hideAnswerButtons() {
-        
+        firstChoice.isHidden = true
+        secondChoice.isHidden = true
+        thirdChoice.isHidden = true
+        fourthChoice.isHidden = true
+        playAgainButton.isHidden = false
     }
     
     //Show buttons
     func showAnswerButtons() {
-        
+        firstChoice.isHidden = false
+        secondChoice.isHidden = false
+        thirdChoice.isHidden = false
+        fourthChoice.isHidden = false
+        playAgainButton.isHidden = true
     }
     
     //disable answer buttons
     func disableAnswerButtons() {
-        
+        firstChoice.isEnabled = false
+        secondChoice.isEnabled = false
+        thirdChoice.isEnabled = false
+        fourthChoice.isEnabled = false
+        playAgainButton.isEnabled = true
     }
     
     //Enable answer buttons
     func enableAnswerbuttons() {
-        
+        firstChoice.isEnabled = true
+        secondChoice.isEnabled = true
+        thirdChoice.isEnabled = true
+        fourthChoice.isEnabled = true
+        playAgainButton.isEnabled = false
     }
     
 
