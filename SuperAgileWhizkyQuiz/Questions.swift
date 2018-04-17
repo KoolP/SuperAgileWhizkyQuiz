53a7c1f5cc1657cdb39d2c37bc545b28745cf2b2
Milja V <mmcvirtanen@gmail.com>
2018-04-17 14:55:42 +0200

    Array of dictionaries (one dict so far)

--- SuperAgileWhizkyQuiz/Questions.swift
+++ SuperAgileWhizkyQuiz/Questions.swift
@@ -8,11 +8,14 @@
 
 import Foundation
 
+//Array of dictionaries
 let questions = [
-                "Question": "Laphroaig Distillery produces a special Single Malt that is matured in special, smaller casks. What is this whisky called?"
+                [
+                "Question": "Laphroaig Distillery produces a special Single Malt that is matured in special, smaller casks. What is this whisky called?",
                 "Answer01": "Tripple Wood",
                 "Answer02": "Quarter Cask",
                 "Answer03": "Select",
                 "Answer04": "Cask Strength",
                 "Correct": "Quarter Cask"
+                ]
                 ]
